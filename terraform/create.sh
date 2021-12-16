92cfb24cc8842630b88b1919294b0bc9373a5189
Matt Cuneo <m@jcuneo.com>
2021-12-17 09:17:17 +1100

    Update create.sh

--- terraform/create.sh
+++ terraform/create.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ $# -ne 7 ]; then
-echo 'Requires 7 arguments: name prefix count cores memory storage size'
-exit 1
+  echo 'Requires 7 arguments: name prefix count cores memory storage size'
+  exit 1
 fi
+
 name="$1"
 shift
 prefix="$1"
@@ -16,6 +17,7 @@ shift
 storage="$1"
 shift
 size="$1"
+
 echo name: $name
 echo prefix: $prefix
 echo count: $count
@@ -26,15 +28,16 @@ echo size: $size
 read  -n 1 -p "Continue? (y/n)" inp
 
 if [ "$inp" = "y" ] || [ "$inp" = "Y" ]; then
-
-cp ./vm.tf.template "./$name.tf"
-sed -i "s/~name~/$name/g" "./$name.tf"
-sed -i "s/~prefix~/$prefix/g" "./$name.tf"
-sed -i "s/~count~/$count/g" "./$name.tf"
-sed -i "s/~cores~/$cores/g" "./$name.tf"
-sed -i "s/~memory~/$memory/g" "./$name.tf"
-sed -i "s/~storage~/$storage/g" "./$name.tf"
-sed -i "s/~size~/$size/g" "./$name.tf"
+  echo creating resource
+  cp ./vm.tf.template "./$name.tf"
+  sed -i "s/~name~/$name/g" "./$name.tf"
+  sed -i "s/~prefix~/$prefix/g" "./$name.tf"
+  sed -i "s/~count~/$count/g" "./$name.tf"
+  sed -i "s/~cores~/$cores/g" "./$name.tf"
+  sed -i "s/~memory~/$memory/g" "./$name.tf"
+  sed -i "s/~storage~/$storage/g" "./$name.tf"
+  sed -i "s/~size~/$size/g" "./$name.tf"
 else
+  echo cancelled
   exit
 fi
